a6c5fd8157c16a63734e0c4a8ba231836e841755
Nicky Dasmijn <nicky.dasmijn@gmail.com>
2020-08-18 00:58:25 +0200

    Add autobuild files.

---
+++ build-cmd.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")"
+
+# turn on verbose debugging output for logs.
+exec 4>&1; export BASH_XTRACEFD=4; set -x
+# make errors fatal
+set -e
+# bleat on references to undefined shell variables
+set -u
+
+top="$(pwd)"
+stage="$top"/stage
+
+VERSION="2.48.0"
+SOURCE_DIR="${top}/glib-${VERSION}"
+
+FFI_VERSION="3.3"
+FFI_SOURCE_DIR="${top}/libffi-${FFI_VERSION}"
+
+# load autobuild provided shell functions and variables
+case "$AUTOBUILD_PLATFORM" in
+    windows*)
+        autobuild="$(cygpath -u "$AUTOBUILD")"
+    ;;
+    *)
+        autobuild="$AUTOBUILD"
+    ;;
+esac
+source_environment_tempfile="$stage/source_environment.sh"
+"$autobuild" source_environment > "$source_environment_tempfile"
+. "$source_environment_tempfile"
+
+build=${AUTOBUILD_BUILD_ID:=0}
+echo "${VERSION}.${build}" > "${stage}/VERSION.txt"
+
+pushd "$SOURCE_DIR"
+    case "$AUTOBUILD_PLATFORM" in
+
+        # ------------------------ windows, windows64 ------------------------
+        windows*)
+        ;;
+
+        # ------------------------- darwin, darwin64 -------------------------
+        darwin*)
+			;;
+        linux64)
+			FLAGS="-m$AUTOBUILD_ADDRSIZE $LL_BUILD_RELEASE -I${stage}/include"
+			export CFLAGS="${FLAGS}" CXXFLAGS="${FLAGS}"
+			export PKG_CONFIG_PATH=${stage}/lib/pkgconfig
+
+			pushd ${FFI_SOURCE_DIR}
+			# libffi does not seem to work well with --libdir, so those *.a need to be copied later
+			./configure --enable-static --disable-shared --disable-docs --prefix=${stage}
+			make -j 6 && make install && make distclean
+			popd
+
+			echo "Name: libpcre" > ${stage}/lib/pkgconfig/libpcre.pc
+			echo "Description: PCRE library" >> ${stage}/lib/pkgconfig/libpcre.pc
+			echo "Version: 8.35" >> ${stage}/lib/pkgconfig/libpcre.pc
+			echo "Libs: -L${stage}/packages/lib/release -lpcre" >> ${stage}/lib/pkgconfig/libpcre.pc
+			echo "Cflags: -I${stage}/packages/include/pcre" >> ${stage}/lib/pkgconfig/libpcre.pc
+			
+			
+			./configure --enable-static --disable-shared --disable-gtk-doc-html --prefix=${stage} --libdir="$stage/lib/release"
+			make -j 6 && make install && make distclean
+
+			cp -a ${stage}/lib/libffi.* "$stage/lib/release/"
+        ;;
+    esac
+    mkdir -p "$stage/LICENSES"
+    cp COPYING "$stage/LICENSES/glib.txt"
+popd
+
